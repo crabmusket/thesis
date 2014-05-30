621e94fa9e3aaae5b934eb6b7791cacc763566e4
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-05-30 20:37:55 +1000

    Fixed install and upgraded to 64-bit box.

--- Vagrantfile
+++ Vagrantfile
@@ -10,7 +10,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # please see the online documentation at vagrantup.com.
 
   # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = "hashicorp/precise32"
+  config.vm.box = "hashicorp/precise64"
 
   if Vagrant.has_plugin?("vagrant-cachier")
     # Configure cached packages to be shared between instances of the same base box.
--- vm/setup-python.sh
+++ vm/setup-python.sh
@@ -1,28 +1,30 @@
-## Install scipy, numpy, etc.
+echo "============== INSTALLING PYTHON DEPEPDENCIES"
 apt-get build-dep python-scipy matplotlib -y
 apt-get install python-dev python-pip libagg-dev -y
 
 pip install distribute --upgrade
 pip install numpy==1.8.1 \
             scipy==0.14.0 \
-            matplotlib==1.3.1 > install.txt
+            matplotlib==1.3.1
 
-if [ $(python -c "import setuptools" &> /dev/null) ]; then
-	# Python setup
+if ! $(python -c "import setuptools" &> /dev/null); then
+	echo "============== INSTALLING SETUPTOOLS"
 	wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
 	rm ./setuptools-3.5.1.zip
 fi
 
-if [ $(python -c "import control.matlab" &> /dev/null) ]; then
-	## Install SLICOT wrapper
+if ! $(python -c "import slycot" &> /dev/null); then
+	echo "============== INSTALLING SLICOT"
 	git clone git://github.com/avventi/Slycot.git
 	cd Slycot
 	git checkout 5af5f283
 	python setup.py install
 	cd ..
 	rm -r Slycot
+fi
 
-	## Install python-control library
+if ! $(python -c "import control.matlab" &> /dev/null); then
+	echo "============== INSTALLING PYTHON-CONTROL"
 	wget http://sourceforge.net/projects/python-control/files/control-0.6d.tar.gz/download -O control-0.6d.tar.gz
 	tar xzf control-0.6d.tar.gz
 	rm control-0.6d.tar.gz
@@ -32,5 +34,5 @@ if [ $(python -c "import control.matlab" &> /dev/null) ]; then
 	rm -r control-0.6d
 fi
 
-# cvxpy
+echo "============== INSTALLING CVXPY"
 pip install cvxpy==0.2
