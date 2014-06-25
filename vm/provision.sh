1a73e27161b7558f8d643a72a78437b5f9f55682
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-06-25 16:09:39 +1000

    Install TexLive. Does not appear to work.

--- vm/provision.sh
+++ vm/provision.sh
@@ -3,7 +3,19 @@ cd /vagrant/vm
 ## Tools
 apt-get update > /dev/null
 apt-get install git -y > /dev/null
-apt-get install texlive-full -y > /dev/null
+apt-get install python-software-properties -y
+
+# TexLive
+add-apt-repository ppa:texlive-backports/ppa -y
+apt-get update > /dev/null
+apt-get install \
+	texlive \
+	texlive-base \
+	texlive-common \
+	texlive-full \
+	texlive-latex-extra \
+	-y > /dev/null
+apt-get upgrade -y > /dev/null
 
 # Components
 ./setup-python.sh
