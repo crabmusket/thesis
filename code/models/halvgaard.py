34ccbb857b61dee6922016b0fe9b23c511fcad4c
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-10-07 15:25:06 +1100

    Now it's an unfinished LTV model.

--- code/models/halvgaard.py
+++ code/models/halvgaard.py
@@ -1,10 +1,14 @@
 from numpy import array
 
 #TODO LOL
-def model(C, UA, NC):
-    A  = array([[-UA/C]])
-    Bu = array([[1/C]])
-    Bw = array([1/C, -1/C, UA/C])
-    C  = array([[1]])
-    D  = array([[0]])
-    return (A, Bu, Bw, C, D)
+def model(C, UA, NC, \
+        auxEfficiency, \
+        collEfficiency, collArea, sunAngleFactor):
+    def inner(t):
+        A  = array([[-UA/C]])
+        Bu = array([[1/C]])
+        Bw = array([1/C, -1/C, UA/C])
+        C  = array([[1]])
+        D  = array([[0]])
+        return (A, Bu, Bw, C, D)
+    return inner
