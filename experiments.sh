46e25eae7232a3abbb5b8ad681e91f59a7dce96b
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-10-25 17:20:10 +1100

    More words, and added an experiment runner.

---
+++ experiments.sh
@@ -0,0 +1,14 @@
+# Effect of prediction error (overpredicting insolation).
+python -m code.experiments.mpc \
+	--days 7 \
+	--start 2 --end 2 \
+	--width 7 --height 5 \
+	--insolationP data/insolation2.txt \
+	--internals \
+	report/results/mpc-prediction-error
+python -m code.experiments.mpc \
+	--days 7 \
+	--start 2 --end 2 \
+	--width 7 --height 5 \
+	--internals \
+	report/results/mpc-prediction-noerror
