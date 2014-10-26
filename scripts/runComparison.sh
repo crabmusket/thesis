458f34bbd8dc7e61d21ac35f647c6c2aadc9df33
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-10-26 23:11:40 +1100

    Work.

--- scripts/runComparison.sh
+++ scripts/runComparison.sh
@@ -8,7 +8,6 @@ for d in 12,1 12,20 1,1 1,20 6,1 6,20 7,1 7,20; do
 			--start 3 --end 3 \
 			--width 9 --height 6.5 \
 			--horizon 8 \
-			--cost 0.0000005 \
 			report/results/$m-comparison-$1-$2
 	done
 done
